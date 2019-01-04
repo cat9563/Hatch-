c8198ad8c886cf7d6030be3293c8781290129a01
cat9563 <crysatl9563@gmail.com>
2019-01-04 15:42:55 -0500

    adds templates

--- hatchTg/urls.py
+++ hatchTg/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings 
 from django.conf.urls.static import static 
+from core import views
 
 urlpatterns = [
+    path('',views.index, name='home'),
     path('admin/', admin.site.urls),
 ]
 
