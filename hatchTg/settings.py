68124b4d40052af457dd8e4a122aa279c0cd6ca8
cat9563 <crysatl9563@gmail.com>
2019-01-04 12:59:10 -0500

    adds heroku to settings

--- hatchTg/settings.py
+++ hatchTg/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,5 @@ USE_TZ = True
 STATIC_URL = '/static/' #might need to be changed back to /static/css/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 AUTH_USER_MODEL = 'core.User'
+# Activate Django-Heroku.
+django_heroku.settings(locals())
