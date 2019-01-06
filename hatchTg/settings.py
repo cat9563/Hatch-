7df5cb9941e40bd67d69c7b325cb8785397b6edb
Mo Glatz <meagabeth@icloud.com>
2019-01-06 13:49:19 -0500

    Google login enabled, still to do: Facebook. Add terms and privacy pages/links to footer

--- hatchTg/settings.py
+++ hatchTg/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
-    # 'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.google',
     # 'allauth.socialaccount.providers.facebook',
 ]
 
