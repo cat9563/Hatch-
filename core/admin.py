62f3fee6082187cbe6e5e331df76696cee03fe78
Mo Glatz <meagabeth@icloud.com>
2019-01-06 12:26:17 -0500

    x

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from core.models import (User)
-from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.views import (PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetDoneView, PasswordResetForm, PasswordResetView)
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ("username", "email")
 
 
 admin.site.register(User, UserAdmin)
