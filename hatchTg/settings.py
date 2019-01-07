9b5e52608b1ed60832186c4bb28f31783d379643
Mo Glatz <meagabeth@icloud.com>
2019-01-06 20:08:45 -0500

    Implement facebook oauth

--- hatchTg/settings.py
+++ hatchTg/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
     'allauth.socialaccount.providers.google',
-    # 'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.facebook',
 ]
 
 MIDDLEWARE = [
