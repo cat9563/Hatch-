894b13a2b752ac3b0e666d5c23bb3dca1982c4f0
Mo Glatz <meagabeth@icloud.com>
2019-01-09 16:31:47 -0500

    Removed second script call from base.html, new line item appears in goal modal

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -1,17 +1,40 @@
-function addTask() {
-    var checklist = []
-    var taskLine = document.getElementById('checklist-task')
+// create a new line item
+var newEl = document.createElement('li');
+console.log(newEl)
+// create a text node that says 'testing'
+var newText = document.createTextNode('testing');
+// console.log(newText)
+// grab the element 'checklist'
+var position = document.getElementById('checklist');
+// console.log(position)
+// new line item containing the text node that says 'testing'
+var newTask = newEl.appendChild(newText);
+// add new line containing text node that says 'testing' onto element 'checklist'
+position.appendChild(newTask)
 
-    let addTask = document.getElementById('plus-button')
-    if (addTask) {
-        addTask.addEventListener('click', function() {
-            checklist.push(taskLine)
-            console.log('plus button clicked')
-        })
-    }
-}
 
-addTask()
+
+
+
+// function addTask() {
+//     // var checklist = [];
+//     // var taskLine = document.getElementById('checklist-task');
+//     var newEl = document.createElement('li');
+//     var newText = document.createTextNode('second test');
+//     var position = document.getElementById('checklist')[0];
+
+//     let addTask = document.getElementById('plus-button')
+//     if (addTask) {
+//         addTask.addEventListener('click', function() {
+//             newEl.appendChild(newText)
+//             position.appendChild(newEl)
+//             console.log('plus button clicked')
+//         })
+//     }
+// }
+
+// addTask()
+// MO: dont forget to call the function once it's done being written
 
 dragula([document.getElementById("left-defaults"), document.getElementById("right-defaults")]);
 
