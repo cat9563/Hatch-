be73fb604c64602fd2fd9e6f41c8424cc0a57de9
Mo Glatz <meagabeth@icloud.com>
2019-01-09 19:05:38 -0500

    Click +, get new <li> with string -- will later be HTML

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -1,40 +1,59 @@
-// create a new line item
-var newEl = document.createElement('li');
-console.log(newEl)
-// create a text node that says 'testing'
-var newText = document.createTextNode('testing');
-// console.log(newText)
-// grab the element 'checklist'
-var position = document.getElementById('checklist');
-// console.log(position)
-// new line item containing the text node that says 'testing'
-var newTask = newEl.appendChild(newText);
-// add new line containing text node that says 'testing' onto element 'checklist'
-position.appendChild(newTask)
+// // create a new line item
+// var newEl = document.createElement('li');
+// console.log(newEl)
+// // create a text node that says 'testing'
+// var newText = document.createTextNode('testing');
+// // console.log(newText)
+// // grab the element 'checklist'
+// var position = document.getElementById('checklist');
+// // console.log(position)
+// // new line item containing the text node that says 'testing'
+// var newTask = newEl.appendChild(newText);
+// // add new line containing text node that says 'testing' onto element 'checklist'
+// position.appendChild(newTask)
+
+
+var addLine = document.getElementById('plus-button')
+addLine.addEventListener('click', addTask)
 
 
 
+function addTask() {
+    console.log('Beginning of Function')
+    if (addLine) {
+        // addLine.addEventListener('click', function() {
+            var newEl = document.createElement('li');
+            newEl.innerText = 'HALP'
+            // console.log(newEl)
+            // var newText = document.createTextNode('test');
+            // console.log(newText)
+            // var newTask = newEl.appendChild(newText);
+            var position = document.getElementById('checklist');
+        position.appendChild(newEl)
+        
+        
+        // console.log(newTask)
+        console.log('end of function')
+        }}
+        
+
+    // var addLine = document.getElementById('plus-button')
 
 
-// function addTask() {
-//     // var checklist = [];
-//     // var taskLine = document.getElementById('checklist-task');
-//     var newEl = document.createElement('li');
-//     var newText = document.createTextNode('second test');
-//     var position = document.getElementById('checklist')[0];
 
-//     let addTask = document.getElementById('plus-button')
-//     if (addTask) {
-//         addTask.addEventListener('click', function() {
-//             newEl.appendChild(newText)
-//             position.appendChild(newEl)
-//             console.log('plus button clicked')
-//         })
-//     }
-// }
-
-// addTask()
-// MO: dont forget to call the function once it's done being written
-
+function addChecklistTask() {
+return`
+    <li>
+        <div class="input-group mb-3" id='checklist-task'>
+            <div class="input-group-prepend">
+                <div class="input-group-text">
+                <input type="checkbox" aria-label="Checkbox for following text input">
+                </div>
+            </div>
+            <input type="text" class="form-control" aria-label="Text input with checkbox">
+        </div>
+    </li>
+`
+}
 dragula([document.getElementById("left-defaults"), document.getElementById("right-defaults")]);
 
