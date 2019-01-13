458d17d44dfae3779b506cccc5cf43cd74f0ad2d
Mo Glatz <meagabeth@icloud.com>
2019-01-13 13:49:07 -0500

    Add code for dynamic progress bar, not yet customized or linked to tasks.

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -215,6 +215,24 @@ function goalHTML(goal) {
     `
 }
 
+
+function loadProgressBar() {
+    var current_progress = 0;
+    var interval = setInterval(function() {
+        current_progress += 10;
+        $("#dynamic")
+        .css("width", current_progress + "%")
+        .attr("aria-valuenow", current_progress)
+        .text(current_progress + "% Complete");
+        if (current_progress >= 100)
+            clearInterval(interval);
+    }, 1000);
+  };
+
+loadProgressBar()
+
+
+
 function setupCSRFAjax () {
     var csrftoken = Cookies.get('csrftoken')
     // console.log(csrftoken);
