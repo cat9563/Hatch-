d6105961d4bb91123f7368c99e1dbb117d1d2f72
RDavis2005 <RDavis2005@msn.com>
2019-01-16 07:15:11 -0500

    fix API view

--- api/views.py
+++ api/views.py
@@ -13,12 +13,13 @@ from api.serializers import (
     EventSerializer, 
     # ResourceSerializer
 )
+
+from rest_framework import generics
 from rest_framework.reverse import reverse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.permissions import (IsAuthenticatedOrReadOnly, IsAuthenticated)
 from django.views.decorators.csrf import ensure_csrf_cookie
-from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.permissions import (
     BasePermission,
